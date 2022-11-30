1d7a7010c8800968490f6755976632c17c0730bf
Binh <binh.le@fss.com.vn>
2022-11-30 07:32:22 +0700

    api gg map

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
-import { User } from "./User";
-import { Company } from "./Company";
+import { User } from './User';
+import { Company } from './Company';
 
 const user = new User();
 const company = new Company();
 
-console.log(user,company);
+console.log(user, company);
 
-console.log('binhtest');
+new google.maps.Map(document.getElementById('map') as HTMLElement, {
+  zoom: 1,
+  center: {
+    lng: 0,
+    lat: 0,
+  },
+});
